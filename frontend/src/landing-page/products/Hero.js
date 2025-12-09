b3099cdc7586b75930043e22d5b275ad7bccbc5d
devasya <devasyapatel555@gmail.com>
2025-12-09 23:13:06 +0530

    add leftsection in product

--- frontend/src/landing-page/products/Hero.js
+++ frontend/src/landing-page/products/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="container">
+    <div className="container border-bottom p-5">
       <div className="text-center mt-5 p-3">
         <h1>Zerodha Products</h1>
         <h3 className="text-muted mt-3 fs-4">Sleek, modern, and intuitive trading platforms</h3>
