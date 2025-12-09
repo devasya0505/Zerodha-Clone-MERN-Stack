191a41399fb56d317e7f89795f573223cca0c3a0
devasya <devasyapatel555@gmail.com>
2025-12-09 16:58:21 +0530

    add education in landing page

--- frontend/src/landing-page/home/Pricing.js
+++ frontend/src/landing-page/home/Pricing.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Pricing() {
   return (
-    <div className="container">
+    <div className="container ">
       <div className="row">
         <div className="col-4">
-          <h1>Unbeatable pricing</h1>
+          <h1 className=" fs-2">Unbeatable pricing</h1>
           <p>
             We pioneered the concept of discount broking and price transparency
             in India. Flat fees and no hidden charges.
@@ -16,11 +16,13 @@ function Pricing() {
           </a>
         </div>
         <div className="col-2"></div>
-        <div className="col-6">
+        <div className="col-6 mb-5">
           <div className="row text-center">
             <div className="col p-4 border">
               <h1 className="mb-3">₹0</h1>
-              <p>Free equity delivery and<br></br> direct mutual funds</p>
+              <p>
+                Free equity delivery and<br></br> direct mutual funds
+              </p>
             </div>
             <div className="col p-4 border">
               <h1 className="mb-3">₹20</h1>
