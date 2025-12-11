4e1f214d03d4630fa9af16d94f49cbe84aedd62e
devasya <devasyapatel555@gmail.com>
2025-12-12 00:03:02 +0530

    connect db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGO_URL;
 
 const app = express(); //create a new application
 
-app.listen(3001, ()=>{
-    console.log("App Started"); //port=3001, because 3000 is in use
+app.listen(3001, () => {
+  console.log("App Started"); //port=3001, because 3000 is in use
+  mongoose.connect(uri);
+  console.log("DB Connected");
 });
