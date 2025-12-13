69ce483e9002ccdd93b55f36a92ed5bd733c3a6d
devasya <devasyapatel555@gmail.com>
2025-12-13 15:29:10 +0530

    import jest-dom

--- frontend/src/test/Hero.test.js
+++ frontend/src/test/Hero.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Hero from " .. /landing_page/home/Hero";
+import "@testing-library/jest-dom";
+import Hero from "../landing-page/home/Hero";
 
+//add Test Suite
 describe("Hero Component", () => {
   test("renders hero image", () => {
     render(<Hero />);
